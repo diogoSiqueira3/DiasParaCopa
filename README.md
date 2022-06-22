25d19ce51aa7986866be3ffd9939727023231af0
Diogo Siqueira <diogo.siqueira.ext@panonline.com.br>
2022-06-22 16:24:51 -0300

    ajustes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Quantos dias até a copa do mundo de 2022?
 
-Esse é um bot que tuita diariamente quantos dias faltam para o início da copa do mundo de 2022, que utiliza Javascript rodando em uma aplicação no Vercel.
+Esse é um bot que tuita quantos dias faltam para o início da copa do mundo de 2022, que utiliza Javascript e NodeJS rodando em uma função AWS Lambda disparada diariamente por um gatilho AWS EventBridge (CloudWatch Events).
 
 Siga em [@DiasParaCopa22](https://twitter.com/DiasParaCopa22)
--- index.js
+++ index.js
@@ -1,26 +1,18 @@
-const dotenv = require('dotenv')
 const Twit = require("twit");
 const moment = require("moment");
-
-dotenv.config({path:'./config.env'});
-
 const client = new Twit({
-    timeout_ms: 60 * 1000,
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+timeout_ms: 60 * 1000,
+consumer_key: process.env.CONSUMER_KEY,
+consumer_secret: process.env.CONSUMER_SECRET,
+access_token: process.env.ACCESS_TOKEN,
+access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
 
-let aCada24h = 1000*60*1440
-
-tuita();
-setInterval(tuita, aCada24h);
-
-function tuita() {
+exports.handler = async () => { 
+    
     let hoje = moment(new Date());
     let inicioCopa = moment("21/11/2022", "DD/MM/YYYY");
-    let diferencaDias;
+    let diferencaDias; 
     let tweet = null;
     
     if (hoje.isSame(inicioCopa, "day")) {
@@ -36,6 +28,9 @@ function tuita() {
     } else if (diferencaDias === 0) {
         tweet = 'Hoje começa a copa do mundo de 2022!';
     }
-
-    client.post('statuses/update', { status: tweet }, function(err, data, response) {})    
-}
+    
+    if (tweet) {
+      await new Promise(resolve => client.post('statuses/update', { status: tweet }, resolve));
+    }
+    
+};
